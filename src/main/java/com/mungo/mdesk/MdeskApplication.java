d8f0c30ed8c2023eaaa3b2d25f0c482a6e1e9618
wangxx <wxxlily@gmail.com>
2018-04-03 15:15:43 +0800

    提交所有文件

---
+++ src/main/java/com/mungo/mdesk/MdeskApplication.java
@@ -0,0 +1,12 @@
+package com.mungo.mdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdeskApplication.class, args);
+	}
+}
