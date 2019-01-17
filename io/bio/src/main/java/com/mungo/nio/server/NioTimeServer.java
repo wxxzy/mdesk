4e3dd21aebe4745b686d0da5ac2a07e8ce1f59fe
wangxingxiang <wangxingxiang@111.com.cn>
2019-01-17 16:22:02 +0800

    nio
    Signed-off-by: wangxingxiang <wangxingxiang@111.com.cn>

---
+++ io/bio/src/main/java/com/mungo/nio/server/NioTimeServer.java
@@ -0,0 +1,21 @@
+package com.mungo.nio.server;
+
+/**
+ * @author wangxingxiang
+ * @Description
+ * @date 2019/1/17 16:20
+ */
+public class NioTimeServer {
+
+    public static void main(String[] args) {
+        int port = 9001;
+        if(args != null &&args.length > 0){
+            try {
+                port = Integer.valueOf(args[0]);
+            }catch (NumberFormatException e){
+
+            }
+        }
+
+    }
+}
