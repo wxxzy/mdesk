120959105652d3f186a703c70d9ed05f9b12b81c
wangxingxiang <wangxingxiang@111.com.cn>
2019-01-17 14:05:11 +0800

    admin
    Signed-off-by: wangxingxiang <wangxingxiang@111.com.cn>

---
+++ example/demokt/src/main/kotlin/com/example/demo/SecurityPermitAllConfig.kt
@@ -0,0 +1,23 @@
+package com.example.demo
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+
+
+
+/**
+ *
+ *
+ * @author wangxingxiang
+ * @Description
+ * @date 2019/1/16 15:40
+ */
+@Configuration
+class SecurityPermitAllConfig : WebSecurityConfigurerAdapter() {
+    @Throws(Exception::class)
+    override fun configure(http: HttpSecurity) {
+        http.authorizeRequests().anyRequest().permitAll()
+                .and().csrf().disable()
+    }
+}
