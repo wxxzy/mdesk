b04a4170d34f038372687fe8b23d2039264e36a1
wangxingxiang <wangxingxiang@111.com.cn>
2019-01-30 18:38:31 +0800

    udp client
    Signed-off-by: wangxingxiang <wangxingxiang@111.com.cn>

--- io/netty/src/main/java/com/mungo/udp/client/UdpNettyClient.java
+++ io/netty/src/main/java/com/mungo/udp/client/UdpNettyClient.java
@@ -1,17 +1,25 @@
-package com.mungo.udp.server;
+package com.mungo.udp.client;
 
+import com.mungo.udp.server.UdpNettyServerHandler;
 import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.DatagramPacket;
 import io.netty.channel.socket.nio.NioDatagramChannel;
+
+import java.net.InetSocketAddress;
+
+import static io.netty.util.CharsetUtil.UTF_8;
 
 /**
  * @author wangxingxiang
  * @Description
- * @date 2019/1/30 18:11
+ * @date 2019/1/30 18:27
  */
-public class UdpNettyServer {
+public class UdpNettyClient {
     public static void main(String[] args) {
         int port = 9004;
         if(args != null &&args.length > 0){
@@ -22,7 +30,7 @@ public class UdpNettyServer {
             }
         }
 
-        new UdpNettyServer().run(port);
+        new UdpNettyClient().run(port);
     }
 
     private void run(int port) {
@@ -31,10 +39,16 @@ public class UdpNettyServer {
         Bootstrap b = new Bootstrap();
         b.group(group).channel(NioDatagramChannel.class)
                 .option(ChannelOption.SO_BROADCAST,true)
-                .handler(new UdpNettyServerHandler());
+                .handler(new UdpNettyClientHandler());
         try {
-            b.bind(port).sync().channel().closeFuture().await();
-            System.out.println("The udp server started at port: "+ port);
+            Channel ch = b.bind(0).sync().channel();
+            //广播
+            ch.writeAndFlush(new DatagramPacket(Unpooled.copiedBuffer("query",UTF_8),
+                    new InetSocketAddress("255.255.255.255",port))).sync();
+            if(!ch.closeFuture().await(15000)){
+                System.out.println("查询超时");
+            }
+
         } catch (InterruptedException e) {
             e.printStackTrace();
         }finally {
