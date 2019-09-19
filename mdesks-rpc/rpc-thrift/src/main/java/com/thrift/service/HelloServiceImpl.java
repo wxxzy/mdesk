4769246a98a02402b061f20fc8933aa22d469ada
wangxingxiang <wangxingxiang@111.com.cn>
2019-09-19 17:50:54 +0800

    add sentinel
    Signed-off-by: wangxingxiang <wangxingxiang@111.com.cn>

--- mdesks-rpc/rpc-thrift/src/main/java/com/thrift/service/HelloServiceImpl.java
+++ mdesks-rpc/rpc-thrift/src/main/java/com/thrift/service/HelloServiceImpl.java
@@ -3,6 +3,7 @@ package com.thrift.service;
 import org.apache.thrift.TException;
 
 public class HelloServiceImpl implements HelloService.Iface {
+    @Override
     public String sayHello(User user) throws TException {
         return "hello,"+user.getName()+user.getEmail();
     }
