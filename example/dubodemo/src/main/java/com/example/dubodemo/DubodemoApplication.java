8ada5a436a6975b60ce22bae2b37dadb801ee0ba
wangxingxiang <wangxingxiang@111.com.cn>
2019-09-30 15:46:58 +0800

    add influxdb
    Signed-off-by: wangxingxiang <wangxingxiang@111.com.cn>

--- example/dubodemo/src/main/java/com/example/dubodemo/DubodemoApplication.java
+++ example/dubodemo/src/main/java/com/example/dubodemo/DubodemoApplication.java
@@ -1,19 +1,32 @@
 package com.example.dubodemo;
 
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
-//import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
+import com.example.dubodemo.influxdb.InfluxDBDemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Random;
+
+//import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
+
 @SpringBootApplication
+@EnableScheduling
 //@EnableDubboConfiguration
 public class DubodemoApplication {
 
 
     public static void main(String[] args) {
         SpringApplication.run(DubodemoApplication.class, args);
+    }
+
+    @Scheduled(fixedRate = 1000)
+    public void doInsert(){
+        Random random = new Random();
+        InfluxDBDemo.insert(random.nextInt(1000));
     }
 
     @RestController
