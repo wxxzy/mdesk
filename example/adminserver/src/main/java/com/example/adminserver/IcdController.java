009e2dc5668eb8b5defe434147970255cfa5788d
mungo <wxx890321@126.com>
2020-05-11 19:32:43 +0800

    add csv
    Signed-off-by: mungo <wxx890321@126.com>

--- example/adminserver/src/main/java/com/example/adminserver/IcdController.java
+++ example/adminserver/src/main/java/com/example/adminserver/IcdController.java
@@ -1,8 +1,12 @@
 package com.example.adminserver;
 
 import com.csvreader.CsvReader;
+import com.example.adminserver.automodify.ChineseStringMatcherStrtegy;
+import com.example.adminserver.automodify.Context;
+import com.example.adminserver.automodify.MatchingStrategy;
 import com.example.adminserver.dao.IcdModel;
 import com.example.adminserver.service.IcdService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,11 +16,11 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Date;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/icd")
+@Slf4j
 public class IcdController {
     @Autowired
     private IcdService icdService;
@@ -34,6 +38,8 @@ public class IcdController {
     @GetMapping(value = "/match")
     public String match(){
         String filePath = "D:\\workspaces\\java\\mdesks\\example\\adminserver\\src\\main\\resources\\recipe.csv";
+
+        //System.out.println(((ChineseStringMatcherStrtegy) matchingStrategy1).sim());
         try {
             File file =new File(filePath);
             InputStreamReader isr=new InputStreamReader(new FileInputStream(file),"utf-8");
@@ -42,12 +48,30 @@ public class IcdController {
             // 读表头
             csvReader.readHeaders();
 
+            List<IcdModel> list = icdService.findAll();
+            MatchingStrategy matchingStrategy1 = new ChineseStringMatcherStrtegy("","");
+            Context context = new Context(matchingStrategy1);
             // 读内容
             while (csvReader.readRecord()) {
-                // 读一整行
-                System.out.println(csvReader.getRawRecord());
                 // 读该行的某一列
-                //System.out.println(csvReader.get("befer"));
+                //String befer = csvReader.get("befer");
+
+                ((ChineseStringMatcherStrtegy) matchingStrategy1).setOriginString(csvReader.get("befer"));
+
+
+
+                for (IcdModel icdModel : list){
+                    ((ChineseStringMatcherStrtegy) matchingStrategy1).setMatchString(icdModel.getDesc());
+                    context.changeStrategy(matchingStrategy1);
+                    context.executeStrategy();
+
+                    double score = ((ChineseStringMatcherStrtegy) matchingStrategy1).sim();
+
+                }
+
+                // 读一整行
+                String line = csvReader.getRawRecord();
+
 
             }
         } catch (IOException e) {
